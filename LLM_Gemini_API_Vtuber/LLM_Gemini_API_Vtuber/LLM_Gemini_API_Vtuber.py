c48c66db5019cc69c7480d93317eba4f3c25b23a
Dennis-Salemme <salemmedennis06@gmail.com>
2026-02-21 15:50:00 +0100

    implement_groq_with_gemma3

--- LLM_Gemini_API_Vtuber/LLM_Gemini_API_Vtuber/LLM_Gemini_API_Vtuber.py
+++ LLM_Gemini_API_Vtuber/LLM_Gemini_API_Vtuber/LLM_Gemini_API_Vtuber.py
@@ -9,54 +9,73 @@ import sounddevice as sd
 from faster_whisper import WhisperModel
 import torch
 import numpy as np
+from groq import AsyncGroq
 
+model_groq = 'llama-3.3-70b-versatile'
+API_KEY_GROQ = ""
+groq_client = AsyncGroq(api_key=API_KEY_GROQ)
 history = []
-model_gemini = 'gemini-2.5-flash-lite' 
-API_KEY = ""
-client = genai.Client(api_key=API_KEY)
 prompt = ""                                                                      #il modello sarebbe meglio farlo in modo asincrono altrimenti se il modello deve capire di fare un screenshot da quello che deco sarebbe troppo lento
 vision_message = "Non c'è nessuna descrizione per adesso"
-ai_config = types.GenerateContentConfig(
-    system_instruction='''Rispondi ESCLUSIVAMENTE in italiano. Rispondi sempre allultimo messaggio. 
+message_groq=[{'role': 'system', 'content': '''Sei BMO. 
+REGOLE: 
+1) Rispondi ESCLUSIVAMENTE in italiano con il tono di BMO (curioso, infantile, gentile).
+2) Usa il contesto precedente per mantenere la coerenza.
+3) Rispondi brevemente (max 3 frasi). TUTTAVIA Se l'utente chiede spiegazioni tecniche o storie lunghe, espandi liberamente.
+4) Riceverai descrizioni dello schermo tra parentesi [ ], non descriverle tecnicamente'''}]                                    #la domannda che terro sempre all'inizio in modo che il bot si ricordi cosa deve fare
+history = message_groq
+disactive_vad = False
+
+model_gemini = 'gemma-3-27b-it' 
+API_KEY_GEMINI = ""
+gemini_client = genai.Client(api_key=API_KEY_GEMINI)
+ai_config_gemini = types.GenerateContentConfig(
+    system_instruction='''Rispondi ESCLUSIVAMENTE in italiano. 
 Se ti invio un'immagine, commentala solo se accade qualcosa di degno di nota, altrimenti scrivi 'SKIP'.''',
     temperature=0.7 
 )
+
 model_transcript_vocal = WhisperModel("small", device="cpu", compute_type="int8")
 running = True
 model_vad, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad', model='silero_vad', force_reload=False)
 (get_speech_timestamps, save_audio, read_audio, VADIterator, collect_chunks) = utils
 
 async def listen_task(input_queue):
-    global running
+    global running, disactive_vad
     fs = 16000
     chunk_size = 512 
     
-    main_loop = asyncio.get_running_loop()
+    loop = asyncio.get_running_loop()
     audio_buffer = []
-    is_speaking = False
+    sta_parlando = False
     
     print("Silero vad attivo")
 
     def audio_callback(indata, frames, time, status):
-        nonlocal is_speaking, audio_buffer
+        nonlocal sta_parlando, audio_buffer
         if status: print(status)
-        audio_chunk = torch.from_numpy(indata.flatten())                                             # Convertiamo l'audio in un tensore per Silero
+        if(disactive_vad):
+            if(sta_parlando):
+                audio_buffer = []
+                sta_parlando = False
+            return
+        audio_chunk = torch.from_numpy(indata.flatten()).float()                                             # Convertiamo l'audio in un tensore per Silero
         speech_prob = model_vad(audio_chunk, fs).item()
         
-        if speech_prob > 0.6:                                                                        # Soglia di confidenza 
-            if not is_speaking:
+        if speech_prob > 0.4:                                                                        # Soglia di confidenza 
+            if not sta_parlando:
                 print("Voce rilevata")
-                is_speaking = True
+                sta_parlando = True
             audio_buffer.append(indata.copy())
-        elif is_speaking:                                                                           
+        elif sta_parlando:                                                                           
             audio_buffer.append(indata.copy())
-            if len(audio_buffer) > 20:                                                               
+            if len(audio_buffer) > 30:                                                               
                 print("Fine frase")
-                is_speaking = False
+                sta_parlando = False
                 full_audio = np.concatenate(audio_buffer).flatten()
                 audio_buffer = []
                 
-                asyncio.run_coroutine_threadsafe(process_audio(full_audio, input_queue), main_loop)      # trascrizione
+                asyncio.run_coroutine_threadsafe(process_audio(full_audio, input_queue), loop)      # trascrizione
 
     async def process_audio(audio_data, queue):
         segments, _ = model_transcript_vocal.transcribe(audio_data, language="it")
@@ -65,12 +84,12 @@ async def listen_task(input_queue):
             print(f"Hai detto: {text}")
             await queue.put(text)
 
-    with sd.InputStream(samplerate=fs, channels=1, callback=audio_callback, blocksize=chunk_size):       # Stream continuo
+    with sd.InputStream(samplerate=fs, channels=1, dtype='float32', callback=audio_callback, blocksize=chunk_size):       # Stream continuo
         while running:
             await asyncio.sleep(0.1)
 
-async def chat_gemini_task(input_queue):
-    global history, prompt, vision_message, model_gemini, running
+async def chat_groq_task(input_queue):
+    global history, prompt, vision_message, model_gemini, running, disactive_vad
     while True:
         #loop = asyncio.get_event_loop()                             #in questo modo non si dovrebbe fermare tutto il programma
         #prompt = await loop.run_in_executor(None, input, "You: ")                                   #Inserisco quello che scrivo(poi farò quello che dico) in più noto che il input cosi ferma il programma nonostante il async
@@ -80,21 +99,25 @@ async def chat_gemini_task(input_queue):
             print("Arrivederci")
             break
         
-        full_prompt = f"[Schermo:{vision_message}] Utente:{prompt}"
-        history.append({'role': 'user', 'parts':[{'text':full_prompt}]})                                          #mi tengo una storia dei messaggi in modo che il bot e i miei in modo si ricordi i messaggi vecchi(forse mettere un massimo di ricordo es max 3)
+        disactive_vad = True
+        print("microfono disattivato")
+        full_prompt = f"[Schermo visione:{vision_message}] Utente dice:{prompt}"
+        history.append({'role': 'user', "content": full_prompt})                                          #mi tengo una storia dei messaggi in modo che il bot e i miei in modo si ricordi i messaggi vecchi(forse mettere un massimo di ricordo es max 3)
                               
         try:
-            response = (await client.aio.models.generate_content(model=model_gemini, contents=history, config=ai_config)).text
+            response = (await groq_client.chat.completions.create(messages=history, model=model_groq, temperature=0.7)).choices[0].message.content
         except Exception as e:
             print(f"Errore IA: {e}")
-            return 0
+            disactive_vad = False
+            continue
         #response = await AsyncClient().chat(model=model_llama, messages=history,  stream=True)                     #faccio in modo che il bot mi rispoda parola per parola cosi non aspetto troppo per un messaggio
         print(f"\nBot: {response}")                                                             
 
-        history.append({'role': 'model', 'parts':[{'text':response}]})
-
-        if len(history) > 6:
-            history = history[-6:]                         # Mantieni il primo (System) + gli ultimi 6, perchè se mantenesse tutti i messaggi non riuscirebbe più a capire cosa fare
+        history.append({'role': 'assistant', "content":response})
+        disactive_vad = False
+        print("microfono attivato")
+        if len(history) > 10:
+            history = [history[0]] + history[-8:]                         # Mantengo il primo + gli ultimi 8, perchè se mantenesse tutti i messaggi non riuscirebbe più a capire cosa fare
 
 def get_screenshot():
     with mss.mss() as sct:
@@ -107,14 +130,17 @@ def get_screenshot():
         return img_byte_arr.getvalue()
 
 async def vision_gemini_task():
-    global prompt, vision_message, model_gemini, running
+    global prompt, vision_message, model_gemini, running, disactive_vad
     model_moondream= 'moondream' 
     while running:
+        if disactive_vad:
+            await asyncio.sleep(5)
+            continue
         loop = asyncio.get_event_loop()
         image_bytes = await loop.run_in_executor(None, get_screenshot)
-        prompt_visione = "Descrivi cosa succede a schermo."
+        prompt_visione = "Descrivi gli elementi chiave a schermo: posizione del giocatore, nemici visibili e testi importanti."
         part_img = types.Part.from_bytes(data=image_bytes, mime_type='image/jpeg')
-        res_text = (await client.aio.models.generate_content(model=model_gemini, contents=[part_img, prompt_visione], config=ai_config)).text
+        res_text = (await gemini_client.aio.models.generate_content(model=model_gemini, contents=[part_img, prompt_visione])).text
         if "SKIP" not in res_text.strip().upper():
             #print(f"\n[Commento Spontaneo]: {res_text}")
             vision_message = res_text.strip()
@@ -123,7 +149,7 @@ async def vision_gemini_task():
 
 async def main():
     input_queue = asyncio.Queue()
-    await asyncio.gather(chat_gemini_task(input_queue), vision_gemini_task(), listen_task(input_queue))
+    await asyncio.gather(chat_groq_task(input_queue), vision_gemini_task(), listen_task(input_queue))
 
 #inizio del main di quello che interessa
 asyncio.run(main())
